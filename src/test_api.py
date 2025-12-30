ba0a23e9f8b83b6c937303ea11b95a4c7e122ad3
Kaitlyn <kaitlyn@praekelt.org>
2025-12-30 14:28:08 +0200

    Fix test for main page (missed in commit for working code)

--- src/test_api.py
+++ src/test_api.py
@@ -5,6 +5,7 @@ from .api import app
 
 client = TestClient(app)
 
-def test_get_root_with_no_data():
+def test_get_home_with_no_data():
     response = client.get("/")
     assert response.status_code == 200
+    assert response.json() == {"msg": "Welcome"}
