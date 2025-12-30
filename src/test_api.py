35b75b82a2170ceaefec27f7668f39d9f656e9f9
Kaitlyn <kaitlyn@praekelt.org>
2025-12-30 14:05:18 +0200

    Inital fastapi app build with failing test

---
+++ src/test_api.py
@@ -0,0 +1,10 @@
+import pytest
+from fastapi.testclient import TestClient
+from .api import app
+
+
+client = TestClient(app)
+
+def test_get_root_with_no_data():
+    response = client.get("/")
+    assert response.status_code == 200
