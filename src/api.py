98af90a27661dbbca79caed9ba12283eec2a8cfb
Kaitlyn <kaitlyn@praekelt.org>
2025-12-31 16:03:53 +0200

    Update home endpoint to handle high cards and test

--- src/api.py
+++ src/api.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
+from typing import Annotated
 
+from fastapi import FastAPI, Query
+
+from .main import *
 
 app = FastAPI()
 
 
 @app.get("/")
-async def get_home():
-    return {"msg": "Welcome"}
+async def get_hand_rating(cards: Annotated[list[str] | None, Query(alias="card")] = None) -> dict:
+    if not cards:
+        return {"msg": "Welcome"}
+
+    hand = cards
+    hand_info = extract_hand_info(hand)
+
+    return {"msg": f'High Card: {high_card(hand_info["values"])}'}
--- src/test_api.py
+++ src/test_api.py
@@ -4,7 +4,12 @@ from .api import app
 
 client = TestClient(app)
 
-def test_get_home_with_no_data():
+def test_home_with_no_data():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Welcome"}
+
+def test_home_with_high_card_hand():
+    response = client.get("/?card=4H&card=6H&card=JD&card=3H&card=QH")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "High Card: Q"}
