86ba285f84f6e89d1e1ef0897ce1abb3a76a90e9
Kaitlyn <kaitlyn@praekelt.org>
2026-01-02 11:38:53 +0200

    Display errors from the api on the frontend

--- frontend/src/app/poker-hand-ranking.ts
+++ frontend/src/app/poker-hand-ranking.ts
@@ -8,8 +8,24 @@ export class PokerHandRanking {
   url = 'http://localhost:8000/';
 
   async submitHand(cards: string[]): Promise<RankingResult | undefined> {
-    console.log(cards.join('&cards='));
-    const ranking = await fetch(`${this.url}?cards=${cards.join('&cards=')}`);
-    return ranking.json()
+    try {
+      const response = await fetch(`${this.url}?cards=${cards.join('&cards=')}`);
+      if (!response.ok){
+        // NOTE: This error handling isn't great. Ideally we'd display this next to the
+        // correct input field, and the message would be more user friendly.
+        // For now we just concat all the errors and display them instead of the result
+        const errorBody = await response.json().catch(() => null);
+        var msg = ""
+        for (const err of errorBody.detail) {
+          msg += "\n" + err.msg
+        }
+        return {"msg": msg};
+      }
+      const ranking = await response.json()
+      return ranking;
+    } catch (error) {
+      console.log(error)
+      return {"msg": "An error occured. Please try again later."}
+    }
   };
 }
